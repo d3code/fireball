ee73b8c547d7d30aa8a1326e6279b48b60f2b628
Luke Sands <101636398+luk3sands@users.noreply.github.com>
2024-06-17 09:16:42 +1000

    refactor: Implement RouteWs method to handle WebSocket routes and actions (#1)

--- fireball.go
+++ fireball.go
@@ -1,8 +1,10 @@
 package fireball
 
 import (
+    "log"
     "log/slog"
     "net/http"
+    "strings"
 )
 
 type Config struct {
@@ -49,5 +51,32 @@ func Default() *Engine {
 }
 
 func (e Engine) Run() error {
-    return http.ListenAndServe(e.Config.Addr, e.mux)
+    return http.ListenAndServe(e.Config.Addr, middlewareCORS(e.mux))
+}
+
+// middlewareCORS is a middlewareLog that adds CORS headers to the response if the Origin header is set
+func middlewareCORS(h http.Handler) http.Handler {
+    interceptor := func(w http.ResponseWriter, r *http.Request) {
+        if origin := r.Header.Get("Origin"); origin != "" {
+
+            w.Header().Set("Access-Control-Allow-Origin", origin)
+
+            if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+                headers := []string{"Content-Type", "Accept", "Authorization", "X-Access-Token", "X-Refresh-Token"}
+                methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+
+                w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+                w.Header().Set("Access-Control-Expose-Headers", strings.Join(headers, ","))
+
+                w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+
+                log.Println("CORS preflight request")
+
+                return
+            }
+        }
+        h.ServeHTTP(w, r)
+    }
+
+    return http.HandlerFunc(interceptor)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/d3code/fireball
 
 go 1.22.2
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	golang.org/x/net v0.25.0
+)
